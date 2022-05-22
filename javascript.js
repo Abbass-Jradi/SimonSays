a4703c62116d4e30badaa7593adb142e3584a6ca
abbas jradi <jradiabbass@gmail.com>
2022-05-21 20:37:35 -0700

    Adding the press any key to start function

--- javascript.js
+++ javascript.js
@@ -6,3 +6,14 @@ var elements = " ";
 var sequenceColors = [];
 var playerColors = [];
 var temp = 0;
+
+/* Press any key to start function */
+window.addEventListener('keydown', start)
+function start() {
+    document.getElementsByTagName('html')[0].style.backgroundColor = "rgb(3, 3, 63)";
+    if (isStarted === false) {
+        document.getElementById('main-prompt').innerHTML = "Level: " + level;
+        generateSequence();
+        isStarted = true;
+    }
+}
