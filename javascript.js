1ec7361acfdf5e31fe2972f338868bf638872364
abbas jradi <jradiabbass@gmail.com>
2022-05-21 20:36:35 -0700

    Setting up the variables

---
+++ javascript.js
@@ -0,0 +1,8 @@
+/* variables section */
+let isStarted = false;
+var level = 0;
+var colors = ['green', 'red', 'yellow', 'blue'];
+var elements = " ";
+var sequenceColors = [];
+var playerColors = [];
+var temp = 0;
